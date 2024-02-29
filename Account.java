8267a6370e2546750c9732dd62d6e9c4fa190ae7
breeze363 <108889227+breeze363@users.noreply.github.com>
2024-02-29 21:39:26 +0800

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,3 +1,31 @@
 public class Account {
+   private double localWithdrawLimit ;
+    private double overseasWithdrawLimit;
+    private int accountnumber;
+    private double initialBalance;
+    private double transferLimit;
+
+
+    public Account(){
+        this.localWithdrawLimit = 3000;
+        this.overseasWithdrawLimit = 3000;
+    }
+    public void displayAccountInfo(){
+
+    }
+
+    public void setTransferLimit(){
+
+    }
+    public void setOverseasWithdrawLimit(){
+
+    }
+    public void deposit(double amount){
+
+    }
+    public void withdraw(double amount){
+
+    }
+
 
 }
