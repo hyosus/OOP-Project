f4a3470e5dd3b2eb8a646c39b28b3d035ca3171e
jimmyboi151 <leongjimmy03@gmail.com>
2024-03-01 16:39:36 +0800

    Update Insurance.java

--- Insurance.java
+++ Insurance.java
@@ -1,3 +1,45 @@
-public class Insurance {
-    private int InsuranceID;
+public class Insurance 
+{
+    private int insuranceID;
+    private String insuranceType;
+    private double coverageAmount;
+    
+    public Insurance(int insuranceID, String insuranceType)
+    {
+        this.insuranceID = insuranceID;
+        this.insuranceType = insuranceType;
+        this.coverageAmount = 5000.00; 
+    }
+    
+
+    public void displayInsuranceDetails()
+    {
+        System.out.println("Insurance ID: " + insuranceID);
+        System.out.println("Type: " + insuranceType);
+        System.out.println("Coverage Amount: " +coverageAmount);
+    }
+
+    public void updateCoverageAmount(double newCoverageAmount)
+    {
+        this.coverageAmount = newCoverageAmount;
+    }
+
+    public boolean isCoverageSufficient(double claimAmount)
+    {
+        return coverageAmount >= claimAmount;
+    }
+
+    public void processClaim(double claimAmount)
+    {
+        if (isCoverageSufficient(claimAmount))
+        {
+            System.out.println("Insurance claim processed successfully");
+            this.coverageAmount -=claimAmount;
+        }
+        else
+        {
+            System.out.println("Insufficient coverage amount. Insurance claim denied");
+        }
+    }
+
 }
