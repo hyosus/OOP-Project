d4f547391516fd064597ff5dc97090f7991876e8
tanys086 <2300895@sit.singaporetech.edu.sg>
2024-03-14 14:27:55 +0800

    account

--- Account.java
+++ Account.java
@@ -1,30 +1,35 @@
+import java.util.Date;
 
 public class Account {
-   private double localWithdrawLimit ;
-   private double overseasWithdrawLimit;
+   private int transactionID;
+   private String accountType;
    private String accountNumber;
    private double balance;
    private double transferLimit;
    private Customer customer;
+   private double withdrawlLimit;
 
    public Account(String accNumber, Customer customer){
-      this.localWithdrawLimit = 3000;
-      this.overseasWithdrawLimit = 3000;
+      // this.transactionID = TID;
+      this.transferLimit = 3000;
+      this.accountType = accountType;
+      this.withdrawlLimit = 3000;
       this.balance = 0;
       this.accountNumber = accNumber;
       this.customer = customer;
 
    }
    
-   
    public void deposit(double amount){
       balance += amount;
       System.out.println("Deposited $" + amount + " into account " + accountNumber);
 
    }
+
+
    public void withdraw(double amount){
-      if (amount > localWithdrawLimit) {
-         System.err.println();
+      if (amount > withdrawlLimit) {
+         System.out.println("You have hit your withdrawal limit. ");
          
       }
       if (balance >= amount) {
@@ -37,18 +42,48 @@ public class Account {
 
    }
 
-   public void transfer(double amount){
+   public void transfer(Account recipient, double amount) {
+      if (amount > transferLimit) {
+         System.out.println("You have hit your Transfer limit. ");
+         
+      }
+      if (balance >= amount) {
 
+         balance -= amount;
+         recipient.deposit(amount);
+         System.out.println("Transaction successful. $" + amount + " transferred to account " + recipient.accountNumber + ".");
+      } else {
+         System.out.println("Transaction failed. Insufficient funds or invalid amount.");
+      }
    }
 
+   
    //display
    public void displayAccountInfo(){
+      System.out.println("~~~~This is your Account Info~~~~");
+      System.out.println("Account Number:" + accountNumber);
+      System.out.println("Account Type:" + accountType);
+      System.out.println("Customer:" + customer);
+      System.out.println("Balance:" + balance);
+      System.out.println("TransferLimit:" + transferLimit );
+      System.out.println("Withdraw Limit:" + withdrawlLimit);
+      System.out.println("~~~~~END~~~~~" );
+   }
+
+   public void displayTransactionInfo(){
+
    }
 
 
    // getter setter
    public double getbalance(){
       return balance;
+   }
+   public String getAccountNumber(){
+      return this.accountNumber;
+   }
+   public String getAccountType(){
+      return this.accountType;
    }
 
    public void setTransferLimit(double amount){
@@ -60,31 +95,37 @@ public class Account {
       return this.transferLimit;
    }
 
-   public void setOverseasWithdrawLimit(double amount){
-      this.overseasWithdrawLimit = amount;
-
+   public void setWithdrawLimit(double amount){
+      this.withdrawlLimit= amount;
    }
-   public double getOverseasWithdrawLimit(){
-      return this.overseasWithdrawLimit;
-   }
-
-   public void setLocalWithdrawLimit(double amount){
-      this.localWithdrawLimit= amount;
-   }
-   public double getLocalWithdrawLimit()
+   public double getWithdrawLimit()
    {
-      return this.localWithdrawLimit;
+      return this.withdrawlLimit;
    }
 
-   
-   
-
-   
+}
 
 
+class Transaction {
+   private String transactionId;
+   private String transactionType;
+   private double amount;
+   private Date date;
+   private String accountNumber;
 
+   public Transaction(String transactionId, String transactionType, double amount, Date date, String accountNumber) {
+      this.transactionId = transactionId;
+      this.transactionType = transactionType;
+      this.amount = amount;
+      this.date = date;
+      this.accountNumber = accountNumber;
+   }
 
-   
-
-
+   public String getDetails() {
+      return "Transaction ID: " + transactionId +
+            ", Type: " + transactionType +
+            ", Amount: " + amount +
+            ", Date: " + date +
+            ", Account Number: " + accountNumber;
+   }
 }
