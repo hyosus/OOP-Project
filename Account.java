cee89c739c6f9c5ffc4b4dde66208bcf306ab859
tanys086 <2300895@sit.singaporetech.edu.sg>
2024-04-06 20:27:07 +0800

    removed transaction class

--- Account.java
+++ Account.java
@@ -37,7 +37,6 @@ public class Account {
 
 
     public Account(String accountID, String accountType, double balance){
-        // this.transactionID = TID;
         this.transferLimit = TRANSFER_LIMIT;
         this.accountType = accountType;
         this.withdrawalLimit = WITHDRAWAL_LIMIT;
@@ -45,7 +44,6 @@ public class Account {
         this.accountID = accountID;
         this.loanList = new ArrayList<>();
         this.transactionList = new ArrayList<>();
-        //allAccounts.add(this);
     }
 
     
@@ -545,64 +543,5 @@ public class Account {
         for (Loan loan : loanList) {
             loan.getLoanInfo();
         }
-    }
-}
-
-
-class Transaction {
-    private String transactionId;
-    private String transactionType;
-    private double amount;
-    private String date;
-    private String accountID;
-    private String recipientAccountID;
-
-
-    public Transaction(String transactionId, String transactionType, double amount, String date, String accountID) {
-        this.transactionId = transactionId;
-        this.transactionType = transactionType;
-        this.amount = amount;
-        this.date = date;
-        this.accountID = accountID;
-    }
-
-    public Transaction(String transactionId, String transactionType, String accountID,String recipientAccountID, double amount, String date) {
-        this.transactionId = transactionId;
-        this.transactionType = transactionType;
-        this.accountID = accountID;
-        this.recipientAccountID = recipientAccountID;
-        this.amount = amount;
-        this.date = date;
-    }
-
-    // Display the details of the transaction
-    public String getDetails() {
-        return "Transaction ID: " + transactionId +
-                ", Type: " + transactionType +
-                ", Amount: " + amount +
-                ", Date: " + date +
-                ", Account ID: " + accountID;
-    }
-
-    public String getTransactionId() {
-        return String.valueOf(this.transactionId);
-    }
-
-
-    public String getTransactionType() {
-        return String.valueOf(this.transactionType);
-    }
-    public double getAmount(){
-        return this.amount;
-    }
-
-    public CharSequence getDate() {
-        return (CharSequence) date;
-    }
-    public String getAccountID(){
-        return String.valueOf(this.accountID);
-    }
-    public String getRecipientAccountID(){
-        return String.valueOf(this.recipientAccountID);
     }
 }
